fdc8d1f952366e93f23238653ce0f2cebe809b1c
Alisa <BangtanGal13@yandex.ru>
2023-11-23 10:04:55 +0300

    Дз 7 номер 1 исправлено

--- Lesson 7/Task 1/Task 1.cpp
+++ Lesson 7/Task 1/Task 1.cpp
@@ -2,10 +2,10 @@
 #include <string>
 #include <cmath>
 
-
-int add(int a, int b) {
-    return a + b;
-}
+// Раньше я писала свою функцию, описывала ее до программы. Сейчас вместо нее воспользовалась библиотечной функцией.
+// int add(int a, int b) {
+//   return a + b;
+// }  
 
 #define MODE 1
 
@@ -21,8 +21,8 @@ int main() {
 #if MODE == 0
     std::cout << "Работаю в режиме тренировки" << std::endl;
 #elif MODE == 1
-    std::cout << "Работаю в боевом режиме" << std::endl;
 
+    std::cout << "Работаю в боевом режиме" << std::endl;
     std::cout << "Введите число 1: ";
     int num1;
     std::cin >> num1;
@@ -30,7 +30,7 @@ int main() {
     int num2;
     std::cin >> num2;
 
-    std::cout << "Результат сложения: " << add(num1, num2);
+    std::cout << "Результат сложения: " << fadd(num1, num2); // fadd - функция сложения, вызывается только если MODE == 1.
 
 #else 
     std::cout << "«Неизвестный режим. Завершение работы»";
